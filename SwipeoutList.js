5070531a77c01aab9744928db2e2fb53361bdbf1
yangguang <yangguang1029@163.com>
2018-01-20 17:45:55 +0800

    format code

--- SwipeoutList.js
+++ SwipeoutList.js
@@ -5,11 +5,11 @@ description: 可以往左边滑出的list
 ************************/
 
 import React, { Component, PureComponent } from 'react';
-import { Text, View, StyleSheet, Dimensions,  FlatList, Animated } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, FlatList, Animated } from 'react-native';
 
-let {width:WIDTH, height:HEIGHT} = Dimensions.get('window')
+let { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
 
-class ListCell extends Component{
+class ListCell extends Component {
 
     constructor(props) {
         super(props);
@@ -17,105 +17,98 @@ class ListCell extends Component{
         this.onStartShouldSetResponder = this.onStartShouldSetResponder.bind(this);
         this.onResponderRelease = this.onResponderRelease.bind(this);
 
-        this.TAG = "CELL" + this.props.number;
-
         this._touchStartPos = null;
-
         this._isOut = false;
         this._isAnimating = false;
 
         this.state = {
-            containerPosX:new Animated.Value(0)
+            containerPosX: new Animated.Value(0)
         }
     }
 
-    _moveOut(){
-        console.log("guangy start move out")
+    _moveOut() {
         this._isAnimating = true;
         Animated.timing(
             this.state.containerPosX,
             {
-              toValue: -40,
+                toValue: -40,
             }
-          ).start(()=>{
-                this._isAnimating = false;
-                this._isOut = true;
-          });
+        ).start(() => {
+            this._isAnimating = false;
+            this._isOut = true;
+        });
     }
 
-    _moveIn(){
+    _moveIn() {
         console.log("guangy start move in")
         this._isAnimating = true;
         Animated.timing(
             this.state.containerPosX,
             {
-              toValue: 0,
+                toValue: 0,
             }
-          ).start(()=>{
+        ).start(() => {
             this._isAnimating = false;
             this._isOut = false;
-      });
+        });
     }
 
-    onStartShouldSetResponder(evt){
-        if(this._isAnimating) {
+    onStartShouldSetResponder(evt) {
+        if (this._isAnimating) {
             return false;
         }
-        console.log("guangy start touch...........")
         this._touchStartPos = evt.nativeEvent.locationX;
         return true;
     }
 
-    onResponderRelease(evt){
-        console.log(this.TAG, "onResponderRelease");
-        if(this._touchStartPos - evt.nativeEvent.locationX > 20 && !this._isOut) {
+    onResponderRelease(evt) {
+        if (this._touchStartPos - evt.nativeEvent.locationX > 20 && !this._isOut) {
             this._moveOut();
-        }else if(this._touchStartPos - evt.nativeEvent.locationX < -20 && this._isOut){
+        } else if (this._touchStartPos - evt.nativeEvent.locationX < -20 && this._isOut) {
             this._moveIn();
         }
     }
 
-    render(){
+    render() {
         let num = this.props.number;
-        let color = num % 2 === 0 ? "red":"green";
-        return <View style={{backgroundColor:color, width:400,height:40}}
+        let color = num % 2 === 0 ? "red" : "green";
+        //用transform:[{translateX:this.state.containerPosX}]也可以
+        return <View style={{ backgroundColor: color, width: 400, height: 40 }}
             onStartShouldSetResponder={this.onStartShouldSetResponder}
             onResponderRelease={this.onResponderRelease}
         >
-        <Animated.View style={{flexDirection:"row", flex:1,position:"absolute", transform:[{translateX:this.state.containerPosX}]}}>
-        <View style={{width:WIDTH,height:40, alignItems:"center"}}>
-        <Text>{num}</Text>
-        </View>
-        <View style={{width:40,height:40, backgroundColor:"gray"}}>
-        </View>
-        </Animated.View>
+            <Animated.View style={{ flexDirection: "row", flex: 1, position: "absolute", left: this.state.containerPosX }}>
+                <View style={{ width: WIDTH, height: 40, alignItems: "center" }}>
+                    <Text>{num}</Text>
+                </View>
+                <View style={{ width: 40, height: 40, backgroundColor: "gray" }}>
+                </View>
+            </Animated.View>
         </View>
     }
 }
 
 
-export default class testSwipeoutList extends Component{
+export default class testSwipeoutList extends Component {
 
     constructor(props) {
         super(props);
-        this.data = [1,2,3,4,5,6]
+        this.data = [1, 2, 3, 4, 5, 6]
 
         this._renderItem = this._renderItem.bind(this);
     }
 
-    _renderItem({item}){
-        return <ListCell number={item}/>
+    _renderItem({ item }) {
+        return <ListCell number={item} />
     }
 
-    render(){
-        return <View style={{flex:1}}>
-
+    render() {
+        return <View style={{ flex: 1 }}>
             <FlatList
                 data={this.data}
                 renderItem={this._renderItem}
-                keyExtractor={(item,index)=>index}
+                keyExtractor={(item, index) => index}
             />
-
         </View>
     }
 }
